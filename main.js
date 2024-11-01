f0dc95095195a198fbf5c3b3a3841ae2e3f8dbd7
Peter <peter.warholm@gmail.com>
2024-11-01 10:23:34 +0100

    adjust naming of functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("DOMContentLoaded", createMain);
 /** Global variables for story and paragraf header, and story image */
 const storyTitle = document.getElementById("storyTitle");
 const storyText = document.getElementById("storyText");
@@ -11,7 +11,7 @@ const north = document.getElementById("north");
 const south = document.getElementById("south");
 
 /** Starting point of program */
-function main() {
+function createMain() {
     console.log("Check Start of program"); // Check program
     setupEventListerners();
     
@@ -24,18 +24,18 @@ function setupEventListerners() {
 }
 
 /** Scene Main One */
-function sceneMainOne() {
+function createSceneMainOne() {
     storyBook.style.display = "grid";
     storyStart.style.display = "none";
     storyImage.src = 'https://picsum.photos/id/15/800/600.webp';
     north.style.display = "none";
     south.style.display = "none";
-    west.onclick = sceneForestOne;
-    east.onclick = sceneCaveOne;
+    west.onclick = createSceneForestOne;
+    east.onclick = createSceneCaveOne;
 };
 
 /** Scene Alt Main One */
-function sceneAltMainOne() {
+function createSceneAltMainOne() {
     storyImage.src = 'https://picsum.photos/id/95/800/600.webp';
     storyTitle.innerText = "The Forest's Edge"
     storyText.innerHTML = '<p>You find yourself awakening to the sound of gentle rustle of leaves.<p>"Where am I?” A voice echoed in your mind.</p><p>You suddenly remember fearsome trolls and mischievous goblins and a legend that speaks of a forgotten realm, hidden deep within these ancient forests, holding treasures beyond imagination.</p><p>“Was it a dream, or a glimpse of something real?”, "Have I been here before?" you wondered, your mind racing.</p><p>A surge of curiosity and a hint of trepidation fill your heart. You know that danger lurks in the shadows, but the allure of the unknown and treasures is too strong to resist.</p>';
@@ -43,29 +43,29 @@ function sceneAltMainOne() {
     south.style.display = "none";
     west.innerText = "West";
     east.innerText = "East";
-    west.onclick = sceneForestOne;
-    east.onclick = sceneCaveOne;
+    west.onclick = createSceneForestOne;
+    east.onclick = createSceneCaveOne;
 };
 
 /** Scene Forest One */
-function sceneForestOne() {
+function createSceneForestOne() {
     storyImage.src = 'https://picsum.photos/id/62/800/600.webp';
     south.style.display = "inline-block";
     storyTitle.innerText = "The Forest is watching";
     storyText.innerHTML = "<p>As you walked further into the forest, you couldn't shake the feeling that you were being watched. The trees seemed to lean in, their gnarled branches reaching out like skeletal fingers. The sunlight, once warm and inviting, now seemed to cast an eerie glow on the forest floor.</p><p>As you start to quickened your pace, hoping to escape the oppressive silence, you see a hooded figure standing in the shadows. The figure stepped into the light, revealing a merchant with a wide grin and a bag full of peculiar items.</p><p>The merchant offered you a variety of magical trinkets, potions, and weapons. He spoke of ancient relics, lost treasures, and forbidden knowledge. You were very intrigued by a certain item. A ring with an Adamas, the most powerful of all gemstones in the fight against evil. However, the merchant's appearance in such a remote and dangerous place was unsettling.</p>";
     west.innerText = "Buy item"
-    west.onclick = sceneForestDeath;
-    east.onclick = sceneAltMainOne;
-    south.onclick = sceneForestTwo;
+    west.onclick = createSceneForestDeath;
+    east.onclick = createSceneAltMainOne;
+    south.onclick = createSceneForestTwo;
 }
 
 /** Scene Cave One */
-function sceneCaveOne() {
+function createSceneCaveOne() {
     storyImage.src = 'https://picsum.photos/id/168/800/600.webp';
 }
 
 /** Scene Forest Death */
-function sceneForestDeath() {
+function createSceneForestDeath() {
     storyImage.src = 'https://picsum.photos/id/53/800/600.webp';
     storyTitle.innerText = "The Merchant's true form";
     storyText.innerHTML = "<p>The ring was a beauty, a glinting circlet of silver and an emerald as green as the forest’s heart. The merchant assured you of its power, its ability to grant wishes.</p><p>As you slipped the ring onto your finger, a strange sensation pulsed through you, a tingling warmth that spread from your hand to your heart. A moment later, a cold, hard sensation enveloped you. Panic set in as you realized you couldn’t move, couldn’t speak. Your body felt heavy, as if it were being pulled into stone.</p><p>You looked down at your hands, horrified to see them turning to stone, the intricate veins and delicate skin hardening into a cold, lifeless form. The process spread upwards, hardening your arms, your chest, your face. As the final tendrils of life were extinguished, you were left a statue, forever trapped in the moment of your transformation.</p><p>The merchant, his true form revealed, watched with a sinister smile. He had found another victim, another soul to add to his growing collection of stone statues.</p>";
